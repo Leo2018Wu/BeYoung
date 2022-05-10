7989858fbb828dbffc4ccb0268e807f9c023adf0
Leo2018Wu <43198067+Leo2018Wu@users.noreply.github.com>
2022-05-10 18:46:02 +0800

    关闭表情弹窗

--- src/bossPages/communication/Session.tsx
+++ src/bossPages/communication/Session.tsx
@@ -322,6 +322,9 @@ const Msgs = ({...props}) => {
             scrollRef.current = e;
             scrollToEnd();
           }}
+          onMomentumScrollBegin={() => {
+            setIsEmojiShow(false);
+          }}
           pt={4}
           px={2}
           mb={5}
