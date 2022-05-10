df647e19f2e31defcda4e1538625adcdef90383d
jiangsh1996 <jiangsh1996@163.com>
2022-05-10 10:47:07 +0800

    alipush依赖集合完成

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {LogBox} from 'react-native';
 import {Provider} from 'react-redux';
 import {NativeBaseProvider, extendTheme, StatusBar} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
+import AliyunPush from 'react-native-aliyun-push';
 
 import Navigation from './src/navigation/Index';
 import colors from './src/theme/bossColor';
@@ -24,6 +25,30 @@ const App = () => {
       },
     },
   });
+  useEffect(() => {
+    // 获取deviceId
+    AliyunPush.getDeviceId()
+      .then(deviceId => {
+        console.log('deviceId:' + deviceId);
+      })
+      .catch(error => {
+        console.log('getDeviceId() failed');
+      });
+
+    //监听推送事件
+    AliyunPush.addListener(handleAliyunPushMessage);
+  }, []);
+
+  const handleAliyunPushMessage = e => {
+    console.log('Message Received. ' + JSON.stringify(e));
+    //e结构说明:
+    //e.type: "notification":通知 或者 "message":消息
+    //e.title: 推送通知/消息标题
+    //e.body: 推送通知/消息具体内容
+    //e.actionIdentifier: "opened":用户点击了通知, "removed"用户删除了通知, 其他非空值:用户点击了自定义action（仅限ios）
+    //e.extras: 用户附加的{key:value}的对象
+  };
+
   return (
     <Provider store={store}>
       <NavigationContainer>
