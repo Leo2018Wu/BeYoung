f925da2e83da9cc8176cbdc12bf9f747f725d528
Leo2018Wu <43198067+Leo2018Wu@users.noreply.github.com>
2022-06-13 10:03:43 +0800

    微信支付android配置

--- android/app/src/main/java/com/zyxsnet/mobile/young/MainApplication.java
+++ android/app/src/main/java/com/zyxsnet/mobile/young/MainApplication.java
@@ -33,6 +33,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.theweflex.react.WeChatPackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -66,7 +67,7 @@ public class MainApplication extends Application implements ReactApplication {
             @SuppressWarnings("UnnecessaryLocalVariable")
             List<ReactPackage> packages = new PackageList(this).getPackages();
             // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
+            packages.add(new WeChatPackage());
             return packages;
         }
 
