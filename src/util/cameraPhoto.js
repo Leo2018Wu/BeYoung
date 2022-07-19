96e4a6fb5167571f7e03a07f96bdb18f1dfd821e
jiangsh1996 <jiangsh1996@163.com>
2022-07-19 16:37:48 +0800

    图片选择权限

--- src/util/cameraPhoto.js
+++ src/util/cameraPhoto.js
@@ -1,34 +1,39 @@
 import ImagePicker from 'react-native-image-crop-picker';
 import {Alert, Linking, Platform} from 'react-native';
-import {check, PERMISSIONS} from 'react-native-permissions';
-
-const checkPermission = async type => {
-  const granted = await check(type);
-  return granted;
-};
+import {check, PERMISSIONS, request} from 'react-native-permissions';
 
 export const openPicker = async (options = {}) => {
   const {maxLength = 9, multiple = false} = options;
   let granted;
   if (Platform.OS === 'ios') {
     granted = await check(PERMISSIONS.IOS.PHOTO_LIBRARY);
+    if (granted !== 'granted') {
+      granted = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
+    }
+  } else {
+    granted = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+    if (granted !== 'granted') {
+      granted = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+    }
   }
-  if (granted === 'blocked') {
-    Alert.alert('相册权限', '相册权限已禁用，是否要去打开权限', [
-      {
-        text: '取消',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {
-        text: '确定',
-        onPress: () => Linking.openURL('app-settings:'),
-        style: 'destructive',
-      },
-    ]);
+  if (granted !== 'granted') {
+    if (Platform.OS === 'ios') {
+      Alert.alert('相册权限', '相册权限已禁用，是否要去打开权限', [
+        {
+          text: '取消',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: '确定',
+          onPress: () => Linking.openURL('app-settings:'),
+          style: 'destructive',
+        },
+      ]);
+    }
   } else {
     const data = await ImagePicker.openPicker({
-      compressImageQuality: 0.8,
+      compressImageQuality: 0.5,
       multiple,
       maxFiles: maxLength,
       mediaType: 'photo',
@@ -41,22 +46,31 @@ export const openPicker = async (options = {}) => {
 
 export const openCamera = async () => {
   let granted;
-  if (Platform.OS === 'ios') {
-    granted = await checkPermission(PERMISSIONS.IOS.CAMERA);
+  granted = await check(
+    Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA,
+  );
+  if (granted !== 'granted') {
+    granted = await request(
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.CAMERA
+        : PERMISSIONS.ANDROID.CAMERA,
+    );
   }
-  if (granted === 'blocked') {
-    Alert.alert('相机权限', '相机权限已禁用，是否要去打开权限', [
-      {
-        text: '取消',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {
-        text: '确定',
-        onPress: () => Linking.openURL('app-settings:'),
-        style: 'destructive',
-      },
-    ]);
+  if (granted !== 'granted') {
+    if (Platform.OS === 'ios') {
+      Alert.alert('相机权限', '相机权限已禁用，是否要去打开权限', [
+        {
+          text: '取消',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: '确定',
+          onPress: () => Linking.openURL('app-settings:'),
+          style: 'destructive',
+        },
+      ]);
+    }
   } else {
     const data = await ImagePicker.openCamera({
       width: 400,
--- src/util/openPicker.js
+++ src/util/openPicker.js
@@ -1,26 +1,35 @@
 import ImagePicker from 'react-native-image-crop-picker';
 import {Alert, Linking, Platform} from 'react-native';
-import {check, PERMISSIONS} from 'react-native-permissions';
+import {check, PERMISSIONS, request} from 'react-native-permissions';
 
-export const openPicker = async (maxFiles = 99, multiple = true) => {
+export const openPicker = async (maxFiles = 9, multiple = true) => {
   let granted;
   if (Platform.OS === 'ios') {
     granted = await check(PERMISSIONS.IOS.PHOTO_LIBRARY);
-    console.log('granted', granted);
+    if (granted !== 'granted') {
+      granted = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
+    }
+  } else {
+    granted = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+    if (granted !== 'granted') {
+      granted = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+    }
   }
-  if (granted === 'blocked') {
-    Alert.alert('相册权限', '相册权限已禁用，是否要去打开权限', [
-      {
-        text: '取消',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {
-        text: '确定',
-        onPress: () => Linking.openURL('app-settings:'),
-        style: 'destructive',
-      },
-    ]);
+  if (granted !== 'granted') {
+    if (Platform.OS === 'ios') {
+      Alert.alert('相册权限', '相册权限已禁用，是否要去打开权限', [
+        {
+          text: '取消',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: '确定',
+          onPress: () => Linking.openURL('app-settings:'),
+          style: 'destructive',
+        },
+      ]);
+    }
   } else {
     const data = await ImagePicker.openPicker({
       compressImageQuality: 0.5,
