d0b1a0d60b5fd1193bb4dc6577ff840d3af9df01
Leo2018Wu <43198067+Leo2018Wu@users.noreply.github.com>
2022-07-21 16:06:56 +0800

    修复女生列表页面筛选问题

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,10 +3,12 @@ import nim from './nim';
 import session from './session';
 import msg from './msg';
 import user from './user';
+import extra from './extra';
 
 export default combineReducers({
   user,
   nim,
   session,
   msg,
+  extra,
 });
